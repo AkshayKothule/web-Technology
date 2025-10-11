b32579e3c4b9a6d0779f49eed940967f49135c06
AkshayKothule <akshaykothule2002@gmail.com>
2025-10-11 17:13:44 +0530

    work on form

---
+++ Javascript/jsdisplayfn.js
@@ -0,0 +1,8 @@
+
+function display(){
+    var v=document.getElementById('mydiv'); //div close tag
+    // console.log(v);
+    v.innerHTML='you clicked the button ';
+    console.log(v);
+
+}
---
+++ form/registrtion.js
@@ -0,0 +1,11 @@
+
+// function validateRadio(){
+
+// }
+<script>
+  function validateForm(){
+
+    
+
+  }
+</script>
